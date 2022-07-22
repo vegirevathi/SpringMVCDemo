3b863c0d2ed0cd6a3d18ada36d760257a82b455d
Revathi <vegirevathi@gmail.com>
2022-07-22 15:40:16 +0530

    This is the demo Spring MVC project to understand and concept of dispatcher servlet and MVC

---
+++ DemoMVCWebProject/src/main/java/com/example/WebConfigurationFile.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@ComponentScan("com.example")
+@Configuration
+public class WebConfigurationFile {
+
+}
---
+++ DemoMVCWebProject/src/main/webapp/index.jsp
@@ -0,0 +1,12 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+
+	<form action="add">
+		Enter number1 : <input type = "text" name = "num1"> <br>
+		Enter number2 : <input type = "text" name = "num2"> <br>
+		<input type = "submit">
+	
+	</form>
+</body>
+</html>
